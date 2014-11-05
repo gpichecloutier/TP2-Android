56f9e3570f21209d26425d75d82afa4afbbefc2f
AlexGodard <alexandre.godard@hotmail.com>
2014-11-05 13:25:58 -0500

    Interface Detail Film

---
+++ app/src/main/java/ca/qc/cstj/android/tp2_android/adapters/HoraireAdapter.java
@@ -0,0 +1,23 @@
+package ca.qc.cstj.android.tp2_android.adapters;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import ca.qc.cstj.android.tp2_android.models.Horaire;
+
+/**
+ * Created by 1247308 on 2014-11-05.
+ */
+public class HoraireAdapter {
+    /*public HoraireAdapter(Context context, int layoutRes, ArrayList<Horaire> horaires) {
+        super(context,layoutRes,horaires);
+    }
+
+    @Override
+    public void update(TextView textView, Horaire object) {
+        String nomComplet = new StringBuilder().append(object.getNom()).append(" ,").append(object.getPrenom()).toString();
+        textView.setText(nomComplet);
+    }*/
+}
